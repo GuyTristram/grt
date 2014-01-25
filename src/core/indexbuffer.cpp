74ea7b7577c5baf4100025cefcfcdc61eede79d5
Guy <guy.tristram@gmail.com>
2014-01-25 10:36:20 +0000

    Added indexbuffer.cpp

---
+++ src/core/indexbuffer.cpp
@@ -0,0 +1,22 @@
+#include "core/indexbuffer.h"
+#include "opengl/opengl.h"
+
+
+unsigned short *IndexBuffer::indices()
+{
+	if( m_dynamic )
+	{
+		glBindBuffer( GL_ELEMENT_ARRAY_BUFFER , 0 );
+		return &m_indices[0];
+	}
+
+	if( !m_gl_buffer )
+	{
+		glGenBuffers( 1, &m_gl_buffer );
+		glBindBuffer( GL_ELEMENT_ARRAY_BUFFER , m_gl_buffer );
+		glBufferData( GL_ELEMENT_ARRAY_BUFFER , m_indices.size() * 2, &m_indices[0], GL_STATIC_DRAW );
+	}
+	else
+		glBindBuffer( GL_ELEMENT_ARRAY_BUFFER , m_gl_buffer );
+	return 0;
+}
